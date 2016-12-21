c4a691e477abc578741425736e1caf219d13dae5
RajeshkumarA_Contus <rajeshkumar.a@contus.in>
2016-12-21 19:43:47 +0530

    PCARPOOLING-9 #time 30 Splash screen structure has been implemented

---
+++ app/src/main/java/com/contus/carpooling/SplashScreenActivity.java
@@ -0,0 +1,74 @@
+/**
+ * @category CarPooling
+ * @package com.contus.carpooling.activity
+ * @copyright Copyright (C) 2016 Contus. All rights reserved.
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.contus.carpooling;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.contus.carpooling.companyregistration.view.CompanyRegistrationActivity;
+
+/**
+ * Show the splash image of the app and  if user not logged then they will be redirected to
+ * company registration screen else they will be redirected to menu_dashboard.
+ *
+ * @author ContusTeam <developers@contus.in>
+ * @version 1.1
+ */
+
+public class SplashScreenActivity extends AppCompatActivity {
+    /**
+     * Used to handle the runnable thread.
+     */
+    private Handler threadHandler;
+
+    /**
+     * Used as a thread
+     */
+    private Runnable mRunnable;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        threadHandler = new Handler();
+        /**
+         * Initializing the thread.
+         */
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent loginActivity = new Intent(SplashScreenActivity.this, CompanyRegistrationActivity.class);
+                startActivity(loginActivity);
+                finish();
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /**
+         *Splash screen timer
+         */
+        int splashTimeOut = 2000;
+        /**
+         * Resume the handler to run the thread when application reopen.
+         */
+        threadHandler.postDelayed(mRunnable, splashTimeOut);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        /**
+         * Stopping the handler when application minimized.
+         */
+        threadHandler.removeCallbacks(mRunnable);
+    }
+}
