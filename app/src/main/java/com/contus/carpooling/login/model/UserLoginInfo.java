c0d9482b8acc28efde146d256306a4bb41612510
RajeshkumarA_Contus <rajeshkumar.a@contus.in>
2016-12-21 19:53:05 +0530

    PCARPOOLING-24 #time 30m Login page valiation has been completed

---
+++ app/src/main/java/com/contus/carpooling/login/model/UserLoginInfo.java
@@ -0,0 +1,68 @@
+/**
+ * @category CarPooling
+ * @package com.contus.carpooling.login.model
+ * @copyright Copyright (C) 2016 Contus. All rights reserved.
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.contus.carpooling.login.model;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.contus.carpooling.BR;
+
+/**
+ * Model class is used to get and store the username and password details
+ *
+ * @author Contus Team <developers@contus.in>
+ * @version 1.0
+ */
+
+public class UserLoginInfo extends BaseObservable {
+
+    /**
+     * Username of the user.
+     */
+    private String userName;
+
+    /**
+     * Password of the user.
+     */
+    private String password;
+
+    /**
+     * Gets {@see #userName}.
+     *
+     * @return {@link #userName}
+     */
+    @Bindable
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * Gets {@see #password}.
+     *
+     * @return {@link #password}
+     */
+    @Bindable
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Sets {@link #userName}.
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+        notifyPropertyChanged(BR.userName);
+    }
+
+    /**
+     * Sets {@link #password}.
+     */
+    public void setPassword(String password) {
+        this.password = password;
+        notifyPropertyChanged(BR.password);
+    }
+}
---
+++ app/src/main/java/com/contus/carpooling/login/viewmodel/LoginController.java
@@ -0,0 +1,81 @@
+/**
+ * @category CarPooling
+ * @package com.contus.carpooling.login.viewmodel
+ * @copyright Copyright (C) 2016 Contus. All rights reserved.
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.contus.carpooling.login.viewmodel;
+
+import android.content.Context;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import com.contus.carpooling.R;
+import com.contus.carpooling.dashboard.DashboardActivity;
+import com.contus.carpooling.login.model.UserLoginInfo;
+import com.contus.carpooling.userregistration.view.UserRegistrationActivity;
+
+/**
+ * Controller of the LoginActivity class
+ * XML view controller
+ *
+ * @author Contus Team <developers@contus.in>
+ * @version 1.0
+ */
+
+public class LoginController {
+
+    /**
+     * OnClick listener of login button
+     *
+     * @param getEditTextValue Used to get the login details
+     * @return OnClickListener of the login button
+     */
+    public View.OnClickListener btnLoginClick(final UserLoginInfo getEditTextValue) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+                if (checkValidation(context, getEditTextValue.getUserName(), getEditTextValue.getPassword()))
+                    context.startActivity(new Intent(context, DashboardActivity.class));
+            }
+        };
+    }
+
+    /**
+     * OnClick listener of category edit text
+     *
+     * @return OnClickListener of the login button
+     */
+    public View.OnClickListener btnSignInOnClick() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.getContext().startActivity(new Intent(view.getContext(), UserRegistrationActivity.class));
+            }
+        };
+    }
+
+    /**
+     * Method used to validate the username and password
+     *
+     * @param context  Used to show the toast message
+     * @param userName Validate the username
+     * @param password Validate the password
+     * @return true when the given field is not empty
+     */
+
+    private boolean checkValidation(Context context, String userName, String password) {
+        boolean validationStatus = true;
+        if (TextUtils.isEmpty(userName) || TextUtils.isEmpty(password)) {
+            validationStatus = false;
+            Toast.makeText(context, "Please make sure username and password field should not be empty", Toast.LENGTH_SHORT).show();
+        } else if (password.length() < 6) {
+            validationStatus = false;
+            Toast.makeText(context, R.string.validation_failure_mobile_length, Toast.LENGTH_SHORT).show();
+        }
+        return validationStatus;
+    }
+}
