2a5feda93a90bbadcc7681e87c91990c6c8931f3
RajeshkumarA_Contus <rajeshkumar.a@contus.in>
2016-12-21 20:01:56 +0530

    PCARPOOLING-153 #time 3h 35m Home page design has been completed with tab layout as basic structure

---
+++ app/src/main/java/com/contus/carpooling/dashboard/DashboardViewAdapter.java
@@ -0,0 +1,75 @@
+/**
+ * @category CarPooling
+ * @package com.contus.carpooling.dashboard
+ * @copyright Copyright (C) 2016 Contus. All rights reserved.
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.contus.carpooling.dashboard;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * The class that contains the list of fragments and list of title. We can use the fragments using
+ * view pager from here.
+ *
+ * @author ContusTeam <developers@contus.in>
+ * @version 2.0
+ */
+public class DashboardViewAdapter extends FragmentPagerAdapter {
+
+    /**
+     * Title of the fragment list
+     */
+    private String[] mTitle;
+
+    /**
+     * Fragment list Which contains the fragments on the adapter
+     */
+    private List<Fragment> fragmentList;
+
+    /**
+     * Instantiates a new adapter dashboard view.
+     *
+     * @param fm the Instance of the FragmentManager
+     */
+    public DashboardViewAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    /**
+     * Set the list of title from the activity
+     *
+     * @param titles Title list
+     */
+    public void setTitle(String[] titles) {
+        this.mTitle = titles;
+    }
+
+    /**
+     * Set the fragment list for the particular view pager using FragmentStatePagerAdapter
+     *
+     * @param fragmentList List of fragment
+     */
+    public void setFragmentList(List<Fragment> fragmentList) {
+        this.fragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mTitle.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitle[position];
+    }
+}
---
+++ app/src/main/java/com/contus/carpooling/dashboard/myrides/MyRidesFragment.java
@@ -0,0 +1,29 @@
+package com.contus.carpooling.dashboard.myrides;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.contus.carpooling.R;
+
+public class MyRidesFragment extends Fragment {
+
+    public MyRidesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_rides, container, false);
+    }
+}
