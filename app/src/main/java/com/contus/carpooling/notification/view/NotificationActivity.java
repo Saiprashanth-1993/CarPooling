9bcd80e7ba16b0f64721b925c64662586c82be67
RajeshkumarA_Contus <rajeshkumar.a@contus.in>
2016-12-28 21:04:19 +0530

    PCARPOOLING-163 #time 3h Notification screen has been completed

---
+++ app/src/main/java/com/contus/carpooling/notification/view/NotificationActivity.java
@@ -0,0 +1,45 @@
+/**
+ * @category CarPooling
+ * @package com.contus.carpooling.notification.view
+ * @copyright Copyright (C) 2016 Contus. All rights reserved.
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.contus.carpooling.notification.view;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.contus.carpooling.R;
+import com.contus.carpooling.databinding.ActivityNotificationBinding;
+
+/**
+ * Activity used to display list of notification details.
+ *
+ * @author Contus Team <developers@contus.in>
+ * @version 1.0
+ */
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityNotificationBinding notificationBinding = DataBindingUtil.setContentView(NotificationActivity.this, R.layout.activity_notification);
+        setSupportActionBar(notificationBinding.toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        NotificationAdapter notificationAdapter = new NotificationAdapter();
+        notificationBinding.notificationItems.setAdapter(notificationAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle toolbar arrow click action
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/contus/carpooling/notification/view/NotificationAdapter.java
@@ -0,0 +1,54 @@
+/**
+ * @category CarPooling
+ * @package com.contus.carpooling.notification.view
+ * @copyright Copyright (C) 2016 Contus. All rights reserved.
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ */
+package com.contus.carpooling.notification.view;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import com.contus.carpooling.R;
+import com.contus.carpooling.databinding.AdapterNotificationBinding;
+
+/**
+ * Adapter class to display the notification list.
+ *
+ * @author Contus Team <developers@contus.in>
+ * @version 1.0
+ */
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder> {
+
+    @Override
+    public NotificationAdapter.NotificationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        AdapterNotificationBinding notificationBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()),
+                R.layout.adapter_notification, parent, false);
+        return new NotificationAdapter.NotificationViewHolder(notificationBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(NotificationAdapter.NotificationViewHolder holder, int position) {
+        /**
+         * Need to implement.
+         */
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    /**
+     * The rides offered list PackageViewHolder for reusable view.
+     */
+    public class NotificationViewHolder extends RecyclerView.ViewHolder {
+        private NotificationViewHolder(AdapterNotificationBinding itemView) {
+            super(itemView.getRoot());
+        }
+    }
+}
