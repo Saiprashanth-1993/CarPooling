fc9b2fc692c8a805681df8bebfb664954f8f68a8
bidushaarumugam <bidu7stars@gmail.com>
2017-03-14 18:40:18 +0530

    PCARPOOLING-66 #time 2hr condition is added with comments

--- app/src/main/java/com/contus/carpooling/changepassword/model/ChangePasswordInfo.java
+++ app/src/main/java/com/contus/carpooling/changepassword/model/ChangePasswordInfo.java
@@ -6,48 +6,98 @@ import com.contus.carpooling.utils.Constants;
 import com.google.gson.annotations.SerializedName;
 
 /**
- * Model class is used to change the password details.
+ * This class will maintain and retrieve the status of corresponding status, value, message and code.
  */
+
 
 public class ChangePasswordInfo extends BaseObservable {
 
+
     /**
-     *
+     *The status
      */
     @SerializedName(Constants.ChangePassword.STATUS)
     public String status;
 
+    /**
+     * The value
+     */
+    @SerializedName(Constants.ChangePassword.VALUE)
+    public String value;
+
+    /**
+     * The message
+     */
+    @SerializedName(Constants.ChangePassword.MESSAGE)
+    public String message;
+
+    /**
+     * The code
+     */
+    @SerializedName(Constants.ChangePassword.CODE)
+    public String code;
+
+    /**
+     * Gets {@see #status}
+     * Returns the status {@link #status}
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Sets {@see #status}
+     * @param status (@link #status}
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
-    @SerializedName(Constants.ChangePassword.VALUE)
-    public String value;
-
+    /**
+     * Gets {@see #value}
+     * Returns the value {@link #value}
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Sets {@see #value}
+     * @param value (@link #value}
+     */
     public void setValue(String value) {
         this.value = value;
     }
 
-
-    @SerializedName(Constants.ChangePassword.MESSAGE)
-    public String message;
-
+    /**
+     * Gets {@see #message}
+     * Returns the message {@link #message}
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Sets {@see #mesage}
+     * @param message (@link #message}
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
-    @SerializedName(Constants.ChangePassword.CODE)
-    public String code;
+    /**
+     * Gets {@see #code}
+     * Returns the code {@link #code}
+     */
+    public String getCode(){
+        return code;
+    }
+
+    /**
+     * Sets {@see #code}
+     * @param code (@link #code}
+     */
+    public void setCode(String code){
+        this.code=code;
+    }
 }
--- app/src/main/java/com/contus/carpooling/changepassword/model/ChangePasswordResponse.java
+++ app/src/main/java/com/contus/carpooling/changepassword/model/ChangePasswordResponse.java
@@ -5,11 +5,17 @@ import com.contus.carpooling.utils.Constants;
 import com.google.gson.annotations.SerializedName;
 
 /**
+ * The model class to get the password change information from the response
  *
+ * @author Contus Team <developers@contus.in>
+ * @version 2.3
  */
 
 public class ChangePasswordResponse extends ErrorResponse {
 
+    /**
+     * The changepassword response set into model.
+     */
     @SerializedName(Constants.ChangePassword.RESPONSE)
     public ChangePasswordInfo password;
 
